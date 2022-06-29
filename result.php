87b2937db161a1595eaf026e37b584ef82ee3cb8
Nana <nanasupriatnakursus@gmail.com>
2022-06-29 14:22:51 +0700

    Update result.php
    updatae

--- result.php
+++ result.php
@@ -1,3 +1,25 @@
 <?php
-    
+    if(isset($_POST['tambah'])) {
+        $namalengkap = $_POST['namalengkap'];
+        $alamat = $_POST['alamat'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $query = mysqli_query($conn, "INSERT INTO temuin VALUES(NULL,'$namalengkap','$alamat','$username','$password','Karyawan')");
+
+        if($query) {
+
+?>
+            <script>
+                alert("Berhasil");
+            </script>
+<?php
+        } else {
+?>
+            <script>
+                alert("Gagal");
+            </script>
+<?php
+        }
+    }
 ?>
