bcfa86e1f63130d884d9369bb51ce0d2860fa5a5
Nana <nanasupriatnakursus@gmail.com>
2022-06-29 14:49:38 +0700

    tes
    tes

--- result.php
+++ result.php
@@ -1,26 +1,18 @@
 <?php
+    include_once("../temuin/connection.php");
     if(isset($_POST['submit'])) {
         $nama = $_POST['nama'];
-        $pilihancardJK = $_POST['pilihancardJK'];
+        $pilihancardJk = $_POST['pilihancardJk'];
         $pilihancardAgama = $_POST['pilihancardAgama'];
         $pilihancardUmur = $_POST['pilihancardUmur'];
         $pilihancardLingkungan = $_POST['pilihancardLingkungan'];
 
-        $query = mysqli_query($conn, "INSERT INTO temuin VALUES(NULL,'$nama','$pilihancardJK','$pilihancardAgama','$pilihancardUmur','$pilihancardLingkungan')");
+        $query = mysqli_query($conn, "INSERT INTO temuin VALUES(NULL,'$nama','$pilihancardJK','$pilihancardAgama','$pilihancardUmur','$pilihancardLingkungan',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)");
 
-        if($query) {
-
-?>
-            <script>
-                alert("Berhasil");
-            </script>
-<?php
-        } else {
-?>
-            <script>
-                alert("Gagal");
-            </script>
-<?php
-        }
+        var_dump($nama);
+        var_dump($pilihancardJK);
+        var_dump($pilihancardAgama);
+        var_dump($pilihancardUmur);
+        var_dump($pilihancardLingkungan);
     }
 ?>
